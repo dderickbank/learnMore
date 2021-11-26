3b76b06527211f2ea2a22898603f0d53ad5a2217
derick bank <derick.bank@integrify.io>
2021-11-26 06:48:44 +0100

    Learning about promises after if statements callbacks

--- asynchronous/async.js
+++ asynchronous/async.js
@@ -5,48 +5,48 @@ let stocks = {
   toppings: ["chocolate", "peanuts"],
 };
 
-let order = (Fruit_name, call_production) => {
-  setTimeout(() => {
-    console.log(`${stocks.Fruits[Fruit_name]} was selected`);
+let isShopOpen = true;
 
-    call_production();
-  }, 2000);
-};
-
-let production = () => {
-  setTimeout(() => {
-    console.log("Production has started");
-
-    setTimeout(() => {
-      console.log("The fruit has been chopped");
-
+let order = (work, time) => {
+  return new Promise((resolve, reject) => {
+    if (isShopOpen) {
       setTimeout(() => {
-        console.log(`${stocks.liquid[0]} and ${stocks.liquid[1]} was added`);
-
-        setTimeout(() => {
-          console.log("the machine has been started");
-
-          setTimeout(() => {
-            console.log(`ice cream was placed on the ${stocks.holder[0]}`);
-
-            setTimeout(() => {
-              console.log(
-                `${stocks.toppings[0]} topping was placed on the ice cream`
-              );
-
-              setTimeout(() => {
-                console.log("serve ice cream");
-              }, 2000);
-            }, 3000);
-          }, 2000);
-        }, 1000);
-      }, 1000);
-    }, 2000);
-  }, 0000);
+        resolve(work());
+      }, time);
+    } else {
+      reject(console.log(`our shop is closed`));
+    }
+  });
 };
 
-order(0, production);
+order(() => console.log(`${stocks.Fruits[1]} was selected`), 2000)
+  .then(() => {
+    return order(() => console.log(`production has started`), 0500);
+  })
 
-// The outcome is that i now understand what callbacks do and how they can be used. This example above would be as we call it a callback hell
-// as it is not very easy on the eye and not clear for yourself nor other developers to read.
-// The solution? Promises.
+  .then(() => {
+    return order(() => console.log(`The fruit was chopped`), 2000);
+  })
+
+  .then(() => {
+    return order(() => console.log(`Water and ice has been added`), 1000);
+  })
+  .then(() => {
+    return order(() => console.log(`The machine has been started`), 1000);
+  })
+  .then(() => {
+    return order(
+      () => console.log(`The ${stocks.holder[1]} has been chosen`),
+      2000
+    );
+  })
+  .then(() => {
+    return order(
+      () => console.log(`${stocks.toppings[1]} has been added`),
+      3000
+    );
+  })
+
+  .finally(() => {
+    return order(() => console.log(`You're icecream is ready. Enjoy!`), 3000);
+  });
