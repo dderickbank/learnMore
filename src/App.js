1c838c03a9883ca9020380bc7a9b473c79931237
derick bank <derick.bank@integrify.io>
2022-01-04 16:51:14 +0100

    added last files

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Tasks from "./components/Tasks";
 import AddTask from "./components/AddTask";
 
 const App = () => {
+  const [showAddTask, setShowAddTask] = useState 
+  (false)
   const [tasks, setTasks] = useState([
     {
       id: 1,
@@ -27,9 +29,10 @@ const App = () => {
 
   // Add Task
   const addTask = (task) => {
-    const id = Math.floor(Math.random() * 10000) + 1;
-
-    console.log(id);
+    const id = Math.floor(Math.random() * 
+    10000) + 1
+    const newTask = { id, ...task}
+    setTasks([...tasks, newTask])
   };
   // Delete task
   const deleteTask = (id) => {
@@ -46,8 +49,10 @@ const App = () => {
   };
   return (
     <div className="container">
-      <Header />
-      <AddTask onAdd={addTask} />
+      <Header onAdd={() => setShowAddTask
+      (!showAddTask)} showAdd={showAddTask}/>
+      {showAddTask && <AddTask onAdd={addTask} />
+}
       {tasks.length > 0 ? (
         <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
       ) : (
